01658a549127d09a6e4b7b71f4a5c3bec7dd6674
tejasmouli <44651691+tejasmouli@users.noreply.github.com>
2020-01-26 03:35:18 +0530

    Changed to use GitHub API as couldn't connect to Twitter API in time. Basic outline done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tweet-dash';
+  topFollowing = 'Top Following';
+  topFollower = 'Top Followers';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { PrimengModule } from './primeng.module';
 import { ProfileComponent } from './profile/profile.component';
+import { FollowComponent } from './follow/follow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    FollowComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
