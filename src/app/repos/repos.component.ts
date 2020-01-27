771c9b1cac17ac77efbb6fbf050c615ff491f414
tejasmouli <44651691+tejasmouli@users.noreply.github.com>
2020-01-27 22:10:27 +0530

    minor change

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GithubService } from '../service/github.service';
 import { Profile } from 'interfaces/profile';
+import { Repo } from 'interfaces/repo';
 
 @Component({
   selector: 'app-repos',
@@ -10,15 +11,15 @@ import { Profile } from 'interfaces/profile';
 })
 export class ReposComponent implements OnInit {
 
-  followings: any;
+  allRepos: Repo[];
 
   constructor(private route: ActivatedRoute, private service: GithubService) { }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((params) => {
       console.log('From repos ' + params.get('user'));
-      this.service.getUserFollowing(params.get('user') ? params.get('user') : 'octocat')
-        .subscribe( res => {this.followings = JSON.stringify(res); console.log(res); }, err => alert);
+      this.service.getUserRepos(params.get('user') ? params.get('user') : 'octocat')
+        .subscribe( res => {this.allRepos = res; console.log(res); }, err => alert);
     });
   }
 
